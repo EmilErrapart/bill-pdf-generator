ebd38520403cf9996c66d2bd2379e89fefcc0e69
Chaostastic <chaostastic.dev@gmail.com>
2024-09-12 10:50:04 +0300

    Copy pdf settings

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,32 @@ use lopdf::content::{Content, Operation};
 
 fn main() {
     let mut doc = Document::with_version("1.7");
-    let pages_id = doc.new_object_id();
 
     let font_id = doc.add_object(dictionary! {
         "Type" => "Font",
         "Subtype" => "Type1",
-        "BaseFont" => "Courier",
+        "BaseFont" => "DejaVuSansCondensed",
+    });
+
+    let font_bold_id = doc.add_object(dictionary! {
+        "Type" => "Font",
+        "Subtype" => "Type1",
+        "BaseFont" => "DejaVuSansCondensed-Bold",
     });
 
     let resources_id = doc.add_object(dictionary! {
+        "ProcSet" => vec!["PDF".into(), "Text".into(), "ImageB".into(), "ImageC".into(), "ImageI".into()],
         "Font" => dictionary! {
             "F1" => font_id,
+            "F2" => font_bold_id,
+        },
+        "ExtGState" => dictionary! {
+            "GS1" => dictionary! {
+                "Type" => "ExtGState",
+                "BM" => "Normal",
+                "ca" => 1,
+                "CA" => 1,
+            },
         },
     });
 
@@ -30,9 +45,19 @@ fn main() {
 
     let content_id = doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
 
+    let pages_id = doc.new_object_id();
+
     let page_id = doc.add_object(dictionary! {
         "Type" => "Page",
         "Parent" => pages_id,
+        "MediaBox" => vec![0.into(), 0.into(), 595.28.into(), 842.89.into()],
+        "TrimBox" => vec![0.into(), 0.into(), 595.28.into(), 842.89.into()],
+        "Resources" => resources_id,
+        "Group" => dictionary! {
+            "Type" => "Group",
+            "S" => "Transparency",
+            "CS" => "DeviceRGB",
+        },
         "Contents" => content_id,
     });
 
@@ -40,8 +65,7 @@ fn main() {
         "Type" => "Pages",
         "Kids" => vec![page_id.into()],
         "Count" => 1,
-        "Resources" => resources_id,
-        "MediaBox" => vec![0.into(), 0.into(), 595.into(), 842.into()],
+        "MediaBox" => vec![0.into(), 0.into(), 595.28.into(), 842.89.into()],
     };
 
     doc.objects.insert(pages_id, Object::Dictionary(pages));
@@ -49,6 +73,8 @@ fn main() {
     let catalog_id = doc.add_object(dictionary! {
         "Type" => "Catalog",
         "Pages" => pages_id,
+        "OpenAction" => vec![page_id.into(), "XYZ".into(), Object::Null, Object::Null, 1.into()],
+        "PageLayout" => "OneColumn",
     });
 
     doc.trailer.set("Root", catalog_id);
