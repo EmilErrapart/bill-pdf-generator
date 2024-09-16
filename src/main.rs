0a0d97d0ed1a29161045a44b4e1866328929930f
Chaostastic <chaostastic.dev@gmail.com>
2024-09-16 13:08:14 +0300

    Total fields

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use wkhtmltopdf::{PageSize, PdfApplication};
+use wkhtmltopdf::{PdfApplication, Size};
 
 fn main() {
     let html = include_str!("templates/template.html");
     let pdf_app = PdfApplication::new().expect("Failed to init PDF application");
     let mut pdfout = pdf_app.builder()
-        .page_size(PageSize::A4)
+        .margin(Size::Millimeters(0))
         .build_from_html(&html)
-        .expect("failed to build pdf");
+        .expect("Failed to build pdf");
 
-    pdfout.save("arve.pdf").expect("failed to save foo.pdf");
+    pdfout.save("arve.pdf").expect("Failed to save pdf");
 }
