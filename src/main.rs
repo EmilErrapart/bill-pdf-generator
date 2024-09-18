d40788ed7f598b9be8f74637394542e3d08627c3
Chaostastic <chaostastic.dev@gmail.com>
2024-09-18 14:07:02 +0300

    Add footer

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,29 @@ fn main() {
     body_context.insert("client_post_index", client.get_post_index());
 
     let body_html = Tera::one_off(include_str!("templates/body.html"), &body_context, false).expect("Failed to render template");
-    let mut body_file = File::create("temp.html").expect("Failed to create temp.html file");
+    let mut body_file = File::create("temp.html").expect("Failed to create temp body.html file");
     body_file.write_all(body_html.as_bytes()).expect("");
     let body_dir = env::current_dir().expect("Failed to get current working directory").join("temp.html");
+
+    let mut footer_context = tera::Context::new();
+    footer_context.insert("company_name", company.get_name());
+    footer_context.insert("company_registry_no", company.get_registry_no());
+    footer_context.insert("company_address", company.get_address());
+    footer_context.insert("company_post_index", company.get_post_index());
+    footer_context.insert("company_city", company.get_city());
+    footer_context.insert("company_country", company.get_country());
+    footer_context.insert("company_kmkr", company.get_kmkr());
+    footer_context.insert("company_email", company.get_email());
+
+    let account = settings.get_account();
+    footer_context.insert("bank_name", account.get_bank_name());
+    footer_context.insert("iban", account.get_iban());
+
+    let phone = settings.get_phone();
+    footer_context.insert("phone_type", phone.get_phone_type());
+    footer_context.insert("phone_no", phone.get_phone_no());
+
+    let footer_html = Tera::one_off(include_str!("templates/footer.html"), &footer_context, false).expect("Failed to render template");
 
     let pdf_options = PrintToPdfOptions {
         landscape: None,
@@ -54,7 +74,7 @@ fn main() {
         page_ranges: None,
         ignore_invalid_page_ranges: None,
         header_template: None,
-        footer_template: None,
+        footer_template: Some(footer_html),
         prefer_css_page_size: None,
         transfer_mode: None,
     };
